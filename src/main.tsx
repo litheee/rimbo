6d827ec347f6d35a32d16781fce6a238ba74989c
litheee <alexxx547@gmail.com>
2023-09-12 03:29:17 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './components'
 import { ThemeProvider } from 'providers'
 
+import './assets/fonts/Inter-Light.ttf'
+import './assets/fonts/Inter-Regular.ttf'
+import './assets/fonts/Inter-Medium.ttf'
+import './assets/fonts/Inter-SemiBold.ttf'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
